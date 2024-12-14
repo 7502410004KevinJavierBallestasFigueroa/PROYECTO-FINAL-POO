39695a678dce664bbc3bf79d09d9703e26704d30
7502410004KevinJavierBallestasFigueroa <kballestasf@unicartagena.edu.co>
2024-12-14 15:17:45 -0500

    Se modifica clase Main
    Se incluye en el método main la aprtura de la ventana principal al correr el proyecto

--- co/edu/unicartagena/Main.java
+++ co/edu/unicartagena/Main.java
@@ -1,12 +1,18 @@
 
 package co.edu.unicartagena;
 
+import co.edu.unicartagena.vista.VentanaPrincipal;
+
 /**
  *
  * @author kevin
  */
 public class Main {
 
+    public static void main(String[] args) {
+        VentanaPrincipal ventanaP = new VentanaPrincipal();
+        ventanaP.setLocationRelativeTo(null);
+        ventanaP.setVisible(true);
     }    
 }
 
